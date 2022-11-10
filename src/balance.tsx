4c09c6d191313ab88b5cdd8e4cf7b4e88e05ecd0
eddyyet <yet.eddy@gmail.com>
2022-11-10 19:02:27 +0800

    UI (Basic)

--- src/balance.tsx
+++ src/balance.tsx
@@ -3,25 +3,70 @@ import './component.css';
 import { NumericFormat } from 'react-number-format';
 import "@fontsource/news-cycle"
 
-export default function Wallet() {
-  const test_balance = 321.123;
-  const test_balance_large = 654321.123456;
+interface Props {
+  CurrencyClass: string;
+  currency: string;
+  BalanceClass: string;
+  balance: number;
+}
+
+export function BalanceBox (props: Props): JSX.Element {
+  // const style = { 'margin-top': '50pt', 'margin-bottom': '0.5rem'};
+  var {
+    CurrencyClass, 
+    currency, 
+    BalanceClass, 
+    balance,
+  } = props;
 
   return (
-    <div className='wallet'>
-      <Grid container>
-        <Grid item xs={12}>
-          <div className='heading'>Wallet</div>
+    <Grid container>
+      <Grid item xs={2} className={CurrencyClass}>
+        {currency}
+      </Grid>
+      <Grid item xs={10} className={BalanceClass}>
+        <NumericFormat displayType="text" value={balance} thousandSeparator="," decimalScale={2} />
+      </Grid>
+    </Grid>
+  );
+}
+
+export function Wallet() {
+  var test_balance = 321.123;
+  var test_balance_large = 654321.123456;
+
+  return (
+    <Stack>
+      <div className='BalanceHeading'>Wallet</div>
+      <Grid container className ='BalanceContent'>
+        <Grid item xs={5}>
+          <BalanceBox CurrencyClass='CurrencyFormat' currency='MMD' BalanceClass='BalanceFormat MMD-color' balance={test_balance} />
         </Grid>
-        <Grid item className='balance-box' xs={6}>
-          <div className='currency'>MMD</div>
-          <div className='balance MMD-color'><NumericFormat displayType="text" value={test_balance} thousandSeparator="," decimalScale={2} /></div>
-        </Grid>
-        <Grid item className='balance-box' xs={6}>
-          <div className='currency'>CMMD</div>
-          <div className='balance CMMD-color'><NumericFormat displayType="text" value={test_balance_large} thousandSeparator="," decimalScale={2} /></div>
+        <Grid item xs={2} />
+        <Grid item xs={5}>
+          <BalanceBox CurrencyClass='CurrencyFormat' currency='CMMD' BalanceClass='BalanceFormat CMMD-color' balance={test_balance_large} />
         </Grid>
       </Grid>
-    </div>
+    </Stack>
+  );
+}
+
+export function Vault() {
+  var test_balance_vault = 21;
+  var test_balance_vault_large = -321.1;
+
+  return (
+    <Stack>
+      <div className='BalanceHeading'>Vault</div>
+      <Grid container className ='BalanceContent'>
+        <Grid item xs={5}>
+          <BalanceBox CurrencyClass='CurrencyFormat' currency='MMD Colleteral' BalanceClass='BalanceFormat MMD-color' balance={test_balance_vault} />
+        </Grid>
+        <Grid item xs={2} />
+        <Grid item xs={5}>
+          <BalanceBox CurrencyClass='CurrencyFormat' currency='CMMD Credited' BalanceClass='BalanceFormat CMMD-color' balance={test_balance_vault_large} />
+        </Grid>
+      </Grid>
+    </Stack>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Stack, Divider } from '@mui/material';
 import './index.css';
 // import App from './App';
-import Wallet from './balance';
+import { Wallet, Vault } from './balance';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +11,14 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <div className = 'root'>
-      <Wallet />
+      <Stack spacing={2} className="main" divider={<Divider orientation="horizontal" color="#666666" flexItem />}>
+        <Wallet />
+        <Vault />
+        <div>Borrow CMMD</div>
+        <div>Repay CMMD</div>
+        <div>Deposit MMD</div>
+        <div>Withdraw MMD</div>
+      </Stack>
     </div>
   </React.StrictMode>
 );
