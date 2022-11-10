9a59117fe2b94c5f5f94cc3612c431dad3f858cc
eddyyet <yet.eddy@gmail.com>
2022-11-11 01:53:26 +0800

    UI (MMD eqv. + collateral ratio)

--- src/balance.tsx
+++ src/balance.tsx
@@ -12,35 +12,45 @@ interface Props {
 }
 
 function BalanceBox (props: Props): JSX.Element {
-  var {
-    CurrencyClass, 
-    currency, 
-    BalanceClass, 
-    balance,
-  } = props;
-
   return (
     <Grid container>
-      <Grid item xs={2} className={CurrencyClass}>
-        {currency}
+      <Grid item xs={2} className={props.CurrencyClass}>
+        {props.currency}
       </Grid>
-      <Grid item xs={10} className={BalanceClass}>
-        <NumericFormat displayType="text" value={balance} thousandSeparator="," decimalScale={2} />
+      <Grid item xs={10} className={props.BalanceClass}>
+        <NumericFormat displayType="text" value={props.balance} thousandSeparator="," decimalScale={2} />
       </Grid>
     </Grid>
   );
 }
 
-function MMDEquivalent (CMMD: number, ExRate:number): JSX.Element {
-  var MMD = CMMD / ExRate;
+function MMDEquivalent (props: {CMMD: number, ExRate:number}): JSX.Element {
+  var MMD = props.CMMD / props.ExRate;
   return (
-    <span className='note'>≈{MMD}</span>
+    <span className='note'>≈ MMD <NumericFormat displayType="text" value={MMD} thousandSeparator="," decimalScale={2} /></span>
+  );
+}
+
+function CollateralRatio (props: {MMD:number, CMMD: number, ExRate:number}): JSX.Element {
+  var CollateralRatio: number;
+
+  if (props.CMMD < 0 && props.MMD >= 0) {
+    CollateralRatio = - props.MMD / props.CMMD * props.ExRate * 100;
+  } else {
+    CollateralRatio = 0;
+  }
+
+  return (
+    <span className='note'>Collateral ratio: <NumericFormat displayType="text" value={CollateralRatio} thousandSeparator="," decimalScale={0} />%</span>
   );
 }
 
 export function WalletDisplay() {
   var test_balance = 321.123;
   var test_balance_large = 654321.123456;
+  var MMD = test_balance;
+  var CMMD = test_balance_large;
+  var ExRate = 50;
 
   return (
     <Stack>
@@ -49,12 +59,13 @@ export function WalletDisplay() {
       </div>
       <Grid container className ='BalanceBoxContent'>
         <Grid item xs={5}>
-          <BalanceBox CurrencyClass='CurrencyFormat' currency='MMD' BalanceClass='BalanceFormat BalancePrimary MMD-color' balance={test_balance} />
+          <BalanceBox CurrencyClass='CurrencyFormat' currency='MMD' BalanceClass='BalanceFormat BalancePrimary MMD-color' balance={MMD} />
         </Grid>
         <Grid item xs={2} />
         <Grid item xs={5}>
-          <BalanceBox CurrencyClass='CurrencyFormat' currency='CMMD' BalanceClass='BalanceFormat BalancePrimary CMMD-color' balance={test_balance_large} />
+          <BalanceBox CurrencyClass='CurrencyFormat' currency='CMMD' BalanceClass='BalanceFormat BalancePrimary CMMD-color' balance={CMMD} />
         </Grid>
+        <Grid item xs={12} className='note'><MMDEquivalent CMMD={CMMD} ExRate={ExRate} /></Grid>
       </Grid>
     </Stack>
   );
@@ -63,6 +74,9 @@ export function WalletDisplay() {
 export function VaultDisplay() {
   var test_balance_vault = 21;
   var test_balance_vault_large = -321.1;
+  var MMD = test_balance_vault;
+  var CMMD = test_balance_vault_large;
+  var ExRate = 50;
 
   return (
     <Stack>
@@ -71,13 +85,15 @@ export function VaultDisplay() {
       </div>
       <Grid container className ='BalanceBoxContent'>
         <Grid item xs={5}>
-          <BalanceBox CurrencyClass='CurrencyFormat' currency='MMD Colleteral' BalanceClass='BalanceFormat BalanceSecondary MMD-color' balance={test_balance_vault} />
+          <BalanceBox CurrencyClass='CurrencyFormat' currency='MMD Colleteral' BalanceClass='BalanceFormat BalanceSecondary MMD-color' balance={MMD} />
         </Grid>
         <Grid item xs={2} />
         <Grid item xs={5}>
-          <BalanceBox CurrencyClass='CurrencyFormat' currency='CMMD Credited' BalanceClass='BalanceFormat BalanceSecondary CMMD-color' balance={test_balance_vault_large} />
+          <BalanceBox CurrencyClass='CurrencyFormat' currency='CMMD Credited' BalanceClass='BalanceFormat BalanceSecondary CMMD-color' balance={CMMD} />
         </Grid>
-        <Grid item xs={12} className='note'><span className='icon'><ErrorOutlineOutlined sx={{ fontSize: 12.8 }} /></span> Your MMD would be liquidated if the collateral ratio is below XX%.</Grid>
+        <Grid item xs={12} className='note'><MMDEquivalent CMMD={CMMD} ExRate={ExRate} /></Grid>
+        <Grid item xs={12} className='note'><CollateralRatio MMD={MMD} CMMD={CMMD} ExRate={ExRate} /></Grid>
+        <Grid item xs={12} className='note'><ErrorOutlineOutlined className='icon_small' sx={{ fontSize: 12.8 }} /> Liquidates when the collateral ratio is below 110%</Grid>
       </Grid>
     </Stack>
   );
