82ed9517e3b5bde9132f14e80afb6b2d2653eb81
eddyyet <yet.eddy@gmail.com>
2022-11-13 15:38:10 +0800

    MetaMask button (draft)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Stack, Divider } from '@mui/material';
+import { MetaMaskProvider } from "metamask-react";
+import { MetaMaskConnection } from './metamask';
 import { WalletDisplay, VaultDisplay } from './balance';
 
 const root = ReactDOM.createRoot(
@@ -9,17 +11,20 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div className = 'root'>
-      <Stack spacing={2} className="main" divider={<Divider orientation="horizontal" color="#666666" flexItem />}>
-        <WalletDisplay />
-        <VaultDisplay />
-        <div>Transfer CMMD</div>
-        <div>Borrow CMMD</div>
-        <div>Repay CMMD</div>
-        <div>Top up MMD</div>
-        <div>Deposit MMD</div>
-        <div>Withdraw MMD</div>
-      </Stack>
-    </div>
+    <MetaMaskProvider>
+      <div className = 'root'>
+        <Stack spacing={2} className="main" divider={<Divider orientation="horizontal" color="#666666" flexItem />}>
+          <MetaMaskConnection />
+          <WalletDisplay />
+          <VaultDisplay />
+          <div>Transfer CMMD</div>
+          <div>Borrow CMMD</div>
+          <div>Repay CMMD</div>
+          <div>Top up MMD</div>
+          <div>Deposit MMD</div>
+          <div>Withdraw MMD</div>
+        </Stack>
+      </div>
+    </MetaMaskProvider>
   </React.StrictMode>
 );
--- src/metamask.tsx
+++ src/metamask.tsx
@@ -1,1 +1,23 @@
-import { Collapse, Button } from '@mui/material';
+// import { CheckCircle } from "@mui/icons-material";
+// import { LoadingButton } from "@mui/lab";
+// import {
+//   Collapse,
+//   Divider,
+//   Stack,
+//   Button,
+//   TextField,
+//   Tooltip,
+//   Typography,
+// } from "@mui/material";
+import { useMetaMask } from "metamask-react";
+
+export function MetaMaskConnection () {
+    const { status, connect, account, chainId, ethereum } = useMetaMask();
+
+    if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
+    if (status === "unavailable") return <div>MetaMask not available :(</div>
+    if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+    if (status === "connecting") return <div>Connecting...</div>
+    if (status === "connected") return <div>Connected account {account} on chain ID {chainId}</div>
+    return null;
+}
