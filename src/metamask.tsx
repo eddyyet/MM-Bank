5f9c167db21a63afe7cfd39a208e865471d8c6a9
eddyyet <yet.eddy@gmail.com>
2022-11-14 02:24:31 +0800

    MetaMask button

--- src/metamask.tsx
+++ src/metamask.tsx
@@ -9,6 +9,8 @@
 //   Tooltip,
 //   Typography,
 // } from "@mui/material";
+import { Button } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 import { useMetaMask } from "metamask-react";
 
 export function MetaMaskConnection () {
@@ -16,8 +18,8 @@ export function MetaMaskConnection () {
 
     if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
     if (status === "unavailable") return <div>MetaMask not available :(</div>
-    if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
-    if (status === "connecting") return <div>Connecting...</div>
-    if (status === "connected") return <div>Connected account {account} on chain ID {chainId}</div>
+    if (status === "notConnected") return <Button variant='outlined' onClick={connect}>Connect to MetaMask</Button>
+    if (status === "connecting") return <LoadingButton loading variant='outlined'>Connecting to Metamask</LoadingButton>
+    if (status === "connected") return <div>Account: {account} | Chain ID: {chainId}</div>
     return null;
 }
