8cba7201712d7b94e28f36a96041a98f8dd280d6
eddyyet <yet.eddy@gmail.com>
2022-11-15 22:17:23 +0800

    MetaMask button (style update)

--- src/metamask.tsx
+++ src/metamask.tsx
@@ -12,14 +12,15 @@
 import { Button } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
 import { useMetaMask } from "metamask-react";
+import './component.css';
 
 export function MetaMaskConnection () {
     const { status, connect, account, chainId, ethereum } = useMetaMask();
 
     if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
     if (status === "unavailable") return <div>MetaMask not available :(</div>
-    if (status === "notConnected") return <Button variant='outlined' onClick={connect}>Connect to MetaMask</Button>
-    if (status === "connecting") return <LoadingButton loading variant='outlined'>Connecting to Metamask</LoadingButton>
+    if (status === "notConnected") return <Button onClick={connect} sx={{borderRadius:'10rem',border:'1px solid #DDDDDD',backgroundColor:'#DDDDDD',color:'#1C1B1F','&:hover':{backgroundColor:'#2C2B2F',color:'#FFFFFF'}}}>Connect to MetaMask</Button>
+    if (status === "connecting") return <LoadingButton loading sx={{borderRadius:'10rem',border:'1px solid #2C2B2F',backgroundColor:'#2C2B2F','& .MuiLoadingButton-loadingIndicator':{color:'#999999'}}}>Connnecting</LoadingButton>
     if (status === "connected") return <div>Account: {account} | Chain ID: {chainId}</div>
     return null;
 }
