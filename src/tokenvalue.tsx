814dab330cf6bc91f9e0b4d1ebb4a592ef0c96f2
eddyyet <yet.eddy@gmail.com>
2022-11-18 01:56:27 +0800

    File rename

---
+++ src/tokenvalue.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export const TokenValue = () => {
+    const [ETD, setETD] = useState<number>(NaN);
+    const [MMDinWallet, setMMDinWallet] = useState<number>(NaN);
+    const [MMDinVault, setMMDinWallet] = useState<number>(NaN);
+    const [CMMDinWallet, setCMMDinWallet] = useState<number>(NaN);
+    const [CMMDinVault, setCMMDinWallet] = useState<number>(NaN);
+    
+}
