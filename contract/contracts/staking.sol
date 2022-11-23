71cd53d0667830e37ec7c35be9c7325519e906aa
Yeung-Chun <anson.yeung.chun@gmail.com>
2022-11-23 12:58:43 +0800

    fixed owner

--- contract/contracts/staking.sol
+++ contract/contracts/staking.sol
@@ -5,15 +5,16 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Staking {
+    address owner;
     mapping(bytes32 => address) public whitelistedTokens;
     mapping(address => mapping(bytes32 => uint256)) public accountBalances;
     
-    // constructor(){
-    //     owner = msg.sender;
-    // }
+    constructor(){
+        owner = msg.sender;
+    }
 
     function whitelistToken(bytes32 symbol, address tokenAddress) external {
-        //require(msg.sender == owner, 'This function is not public');
+        require(msg.sender == owner, 'This function is not public');
         whitelistedTokens[symbol] = tokenAddress;
     }
 
