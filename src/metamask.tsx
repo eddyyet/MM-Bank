ee634dc3bf3a3aaad7d212613f6b0c7a1adf8370
eddyyet <yet.eddy@gmail.com>
2022-12-02 03:47:56 +0800

    UI

--- src/metamask.tsx
+++ src/metamask.tsx
@@ -24,8 +24,8 @@ export default function MetaMaskConnection (): JSX.Element {
 
   if (status === 'initializing') return <div>Synchronisation with MetaMask ongoing...</div>
   if (status === 'unavailable') return <div>MetaMask not available :(</div>
-  if (status === 'notConnected') return <Button onClick={ () => ConnectMetaMask() } sx={{ borderRadius: '2rem', border: '1px solid #DDDDDD', backgroundColor: '#DDDDDD', color: '#1C1B1F', '&:hover': { backgroundColor: '#2C2B2F', color: '#FFFFFF' } }}>Connect to MetaMask</Button>
-  if (status === 'connecting') return <LoadingButton loading sx={{ borderRadius: '2rem', border: '1px solid #2C2B2F', backgroundColor: '#2C2B2F', '& .MuiLoadingButton-loadingIndicator': { color: '#999999' } }}>Connnecting</LoadingButton>
-  if (status === 'connected') return <div><div>Account: {account} | Chain ID: {chainId} | ETD balance: {balance.ETD}</div><div>Exchange Rates -  ETD/MMD: 1000, MMD/CMMD: 5</div></div>
+  if (status === 'notConnected') return <Button onClick={ () => ConnectMetaMask() } sx={{ borderRadius: '2rem', border: '1px solid #DDDDDD', backgroundColor: '#DDDDDD', color: '#0D1117', '&:hover': { backgroundColor: '#1D2127', color: '#FFFFFF' } }}>Connect to MetaMask</Button>
+  if (status === 'connecting') return <LoadingButton loading sx={{ borderRadius: '2rem', border: '1px solid #AAAAAA', background: 'none', '& .MuiLoadingButton-loadingIndicator': { color: '#999999' } }}>Connnecting</LoadingButton>
+  if (status === 'connected') return <div className="AccountInfo"><div>Account: {account} | Chain ID: {chainId} | ETD balance: {balance.ETD}</div><div>Exchange Rates -  ETD/MMD: 1000, MMD/CMMD: 5</div></div>
   return <div />
 }
