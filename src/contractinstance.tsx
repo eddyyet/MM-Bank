4748b3ab91590c0e058051ee3258c4e48c514ce3
eddyyet <yet.eddy@gmail.com>
2022-12-01 23:30:18 +0800

    Contract instance (ESLint)

--- src/contractinstance.tsx
+++ src/contractinstance.tsx
@@ -1,10 +1,10 @@
 import { ethers } from 'ethers'
 import { MMDABI } from './MMD.abi'
-import { CMMDABI} from './CMMD.abi'
+import { CMMDABI } from './CMMD.abi'
 import { IMetaMaskContext } from 'metamask-react/lib/metamask-context'
 
 const MMDAddress = '0x0Ba0bf0F6155774816A32f2983DCB53B12fb6f46'
-const CMMDAddress = '0x7330f402b2abCb4DEC78c0406A065fE9F56AF73e';
+const CMMDAddress = '0x7330f402b2abCb4DEC78c0406A065fE9F56AF73e'
 
 declare global {
   interface Window {
