e7b110324b31234c16537f317625db1e5a81e0a7
eddyyet <yet.eddy@gmail.com>
2022-12-02 03:50:26 +0800

    Cleaning

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -8,9 +8,6 @@ async function main() {
   const MMDaddress = MMDcontract.address;
   console.log("MMD Contract deployed to:", MMDaddress);
 
-  const MMDvalue = await MMDcontract.balanceOf('0x2Efe371AA24D1276B91F8C50C86c2e2D163e966d');
-  console.log("Contract balance:", MMDvalue.toString());
-
   const CMMDContract = await ethers.getContractFactory("CMMDToken");
   const CMMDcontract = await CMMDContract.deploy(MMDaddress);
   await CMMDcontract.deployed();
@@ -19,13 +16,7 @@ async function main() {
   console.log("CMMD Contract deployed to:", CMMDAddress);
 
   await MMDcontract.setCMMDAddress(CMMDAddress);
-
-  const CMMDvalue = await CMMDcontract.balanceOf('0x2Efe371AA24D1276B91F8C50C86c2e2D163e966d');
-  console.log("Contract balance:", CMMDvalue.toString());
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
